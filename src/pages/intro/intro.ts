ed75c6d1765c174ada6a48de06b0cd2f84118346
Ivson Santos <santosivson@hotmail.com>
2018-08-26 15:48:49 -0300

    Projeto atualizado

--- src/pages/intro/intro.ts
+++ src/pages/intro/intro.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { TabsPage } from '../tabs/tabs';
 
 /**
  * Generated class for the IntroPage page.
@@ -15,11 +16,18 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class IntroPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad IntroPage');
+  }
+
+  // funcao criada para navegar de pagina
+  irParaAPagina() {
+    this.navCtrl.push(TabsPage);
   }
 
 }
